259f1905f9bb82a0e1839a2a5564fbe42d60812e
Rohit Singh <singh385536@gmail.com>
2025-10-21 16:29:54 +0530

    push right main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
+
+// src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
